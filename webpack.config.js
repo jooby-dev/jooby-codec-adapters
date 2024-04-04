8cf9220c34125830426e25f9760e143ee0b4f136
Stanislav Kalashnik <darkpark.main@gmail.com>
2024-04-04 13:00:04 +0300

    fix webpack before removal

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,63 @@ export default [
     {
         name: 'chirpstack',
         mode: 'production',
-        entry: './src/chirpstack/index.js',
+        entry: './src/chirpstack/analog/index.js',
         //target: 'es5',
         output: {
             filename: 'webpack.js',
-            path: resolve('./dist/chirpstack/'),
+            path: resolve('./dist/chirpstack/analog/'),
+            chunkFormat: 'array-push',
+            environment: {
+                arrowFunction: false,
+                asyncFunction: false,
+                bigIntLiteral: false,
+                const: false,
+                destructuring: false,
+                dynamicImport: false,
+                dynamicImportInWorker: false,
+                forOf: false,
+                globalThis: false,
+                module: false,
+                optionalChaining: false,
+                templateLiteral: false
+            }
+        },
+        optimization: {
+            minimize: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(?:js|mjs|cjs)$/,
+                    exclude: /node_modules/,
+                    use: {loader: 'babel-loader'}
+                }
+            ]
+        },
+        plugins: [
+            // https://webpack.js.org/plugins/banner-plugin/
+            new webpack.BannerPlugin({
+                raw: true,
+                banner: readFileSync('./src/chirpstack/analog/banner.js', 'utf8'),
+                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
+            }),
+            new webpack.BannerPlugin({
+                raw: true,
+                banner: readFileSync('./src/chirpstack/analog/footer.js', 'utf8'),
+                stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
+                footer: true
+            })
+        ]
+    },
+
+    {
+        name: 'thingsboard',
+        mode: 'production',
+        entry: './src/thingsboard/uplink/index.js',
+        //target: 'es5',
+        output: {
+            filename: 'webpack.js',
+            path: resolve('./dist/thingsboard/'),
             chunkFormat: 'array-push',
             environment: {
                 arrowFunction: false,
@@ -45,12 +97,12 @@ export default [
             // https://webpack.js.org/plugins/banner-plugin/
             new webpack.BannerPlugin({
                 raw: true,
-                banner: readFileSync('./src/chirpstack/init.js', 'utf8'),
+                banner: readFileSync('./src/thingsboard/uplink/banner.js', 'utf8'),
                 stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
             }),
             new webpack.BannerPlugin({
                 raw: true,
-                banner: readFileSync('./src/chirpstack/test.js', 'utf8'),
+                banner: readFileSync('./src/thingsboard/uplink/footer.js', 'utf8'),
                 stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
                 footer: true
             })
